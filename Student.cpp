e8b2828e71d148a523fc6b8ac2e4a1747f8b1d9e
JamesGreyThompson <JamesGreyThompson@users.noreply.github.com>
2018-01-31 12:08:16 -0400

    Added imp and client  file

---
+++ Student.cpp
@@ -0,0 +1,30 @@
+//Student.cpp
+#include "Student.h"
+ 
+Student::Student(double i, string n);
+{
+id = i;
+name = n;
+}
+
+void Student:: setA(i)
+{
+id = i;
+}
+double Student::getA()
+{
+return id;
+}
+
+
+void Student:: setB(i)
+{
+name = n;
+
+}
+double Student::getB()
+{
+return name;
+}
+
+
---
+++ main.cpp
@@ -0,0 +1,17 @@
+//main.cpp
+#include "Student.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main ()
+{
+Student st 
+student st2 = {23,55}
+
+vector <student> s;
+s.push_back(st2);
+s.push_back(st);
+
+void Student::setB
+
