2262b2eb26b71a2fa2ebb071cac9834a77805089
JamesGreyThompson <JamesGreyThompson@users.noreply.github.com>
2018-01-31 11:35:27 -0400

    Added header file

---
+++ Student.h
@@ -0,0 +1,23 @@
+//Student.h
+
+#ifndef STUDENT_H
+#define STUDENT_H
+
+class Student {
+private:
+
+double id;
+string name;
+
+public:
+  Student (double i , string n);
+
+void setA(double i);
+
+double getA();
+
+void setB(string n);
+
+string getB();
+
+}
